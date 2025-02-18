d13555b019e63ac8155a1c9839631fe98d35d57e
donghaozhang <zdhpeter1991@gmail.com>
2025-02-19 01:07:00 +1100

    添加最长上升子序列问题讲解文档和动态规划实现，并更新 README.md 添加 '最长上升子序列 (Longest Increasing Subsequence)'（LeetCode #300）

--- longest_increasing_subsequence.py
+++ longest_increasing_subsequence.py
@@ -1,40 +1,43 @@
 #!/usr/bin/env python
 
-def longest_increasing_subsequence(nums):
-    """Return the longest increasing subsequence from the list of numbers."""
-    if not nums:
-        return []
-    n = len(nums)
-    # dp[i] will be the length of the subsequence ending at index i
-    dp = [1] * n
-    # prev[i] will store the index of the previous element in the subsequence
-    prev = [-1] * n
+class Solution:
+    def longestIncreasingSubsequence(self, nums: list) -> list:
+        if not nums:
+            return []
+        
+        n = len(nums)
+        dp = [1] * n              # dp[i]：以 nums[i] 结尾的最长子序列的长度
+        prev = [-1] * n           # prev[i]：记录前驱索引以便重构子序列
+        
+        max_len = 1
+        max_index = 0
+        
+        for i in range(n):
+            for j in range(i):
+                if nums[j] < nums[i] and dp[j] + 1 > dp[i]:
+                    dp[i] = dp[j] + 1
+                    prev[i] = j
+            if dp[i] > max_len:
+                max_len = dp[i]
+                max_index = i
+        
+        # 重构最长上升子序列
+        lis = []
+        index = max_index
+        while index != -1:
+            lis.append(nums[index])
+            index = prev[index]
+        lis.reverse()
+        return lis
 
-    for i in range(n):
-        for j in range(i):
-            if nums[j] < nums[i] and dp[j] + 1 > dp[i]:
-                dp[i] = dp[j] + 1
-                prev[i] = j
+# 测试代码
+def test_longest_increasing_subsequence():
+    solution = Solution()
+    nums = [4, 5, 6, 1, 2, 3, 5]
+    expected = [1, 2, 3, 5]
+    result = solution.longestIncreasingSubsequence(nums)
+    assert result == expected, f"Expected {expected}, but got {result}"
+    print("所有测试用例通过！")
 
-    # Find the index of the maximum value in dp
-    max_index = 0
-    for i in range(n):
-        if dp[i] > dp[max_index]:
-            max_index = i
-
-    # Reconstruct the subsequence using prev array
-    lis = []
-    while max_index != -1:
-        lis.append(nums[max_index])
-        max_index = prev[max_index]
-    lis.reverse()
-    return lis
-
-
-if __name__ == '__main__':
-    # Example usage
-    nums = [10, 9, 2, 5, 3, 7, 101, 18]
-    lis = longest_increasing_subsequence(nums)
-    print("Input sequence:", nums)
-    print("Longest increasing subsequence:", lis)
-    print("Length of subsequence:", len(lis)) 
+if __name__ == "__main__":
+    test_longest_increasing_subsequence() 
